0d60552973fbe1b080371bb8c8fe1a1d360e1d4f
binRick <4857149+binRick@users.noreply.github.com>
2026-05-26 20:31:26 -0400

    tracep 0.1.3

--- Formula/tracep.rb
+++ Formula/tracep.rb
@@ -1,28 +1,28 @@
 class Tracep < Formula
   desc "Trace a process's network, TLS, DNS, and exec activity"
   homepage "https://github.com/binRick/tracep"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/binRick/tracep/releases/download/v0.1.2/tracep-darwin-arm64"
-      sha256 "a7a50a2bb0e7ec4c290eebfa24e59c8d99fcd31dae327e65bee0cd24810bb677"
+      url "https://github.com/binRick/tracep/releases/download/v0.1.3/tracep-darwin-arm64"
+      sha256 "525fbb4eb0879f30923c561f0e4958c7b77269d6efcc920e320575b041eb7ddd"
     end
     on_intel do
-      url "https://github.com/binRick/tracep/releases/download/v0.1.2/tracep-darwin-amd64"
-      sha256 "5db1f528c810a7711f12cd1728352bc9b4c11fcff46eba3d0b6152285f6aa9a7"
+      url "https://github.com/binRick/tracep/releases/download/v0.1.3/tracep-darwin-amd64"
+      sha256 "6a5c5fec76887ded7c5a0dedfc4d5ae3fae6c1a0245718b9157e97da0a07e3e4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/binRick/tracep/releases/download/v0.1.2/tracep-linux-arm64"
-      sha256 "fd86e7f06d0b2d76d3891f82a421d5d4e9fe06dac1fc71cd50875031b14e1935"
+      url "https://github.com/binRick/tracep/releases/download/v0.1.3/tracep-linux-arm64"
+      sha256 "e374f5913ca3b265d7a22637149b083a1f6f892fed0e5ddf05579fc8aba2c2c1"
     end
     on_intel do
-      url "https://github.com/binRick/tracep/releases/download/v0.1.2/tracep-linux-amd64"
-      sha256 "3b01ba3801f3c0fd6c614757249f76704645d6cedfd34141b6edcaaf4487e732"
+      url "https://github.com/binRick/tracep/releases/download/v0.1.3/tracep-linux-amd64"
+      sha256 "a426c45af3095dfb9c014b9035210e7ddebd2f21db9138daf3f1f57c8c4fa05b"
     end
   end
 
