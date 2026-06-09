4499795d28922e095a26695feffe2023fd3052e4
binRick <4857149+binRick@users.noreply.github.com>
2026-06-09 16:57:45 -0400

    rbterm 0.3.39

--- Casks/rbterm.rb
+++ Casks/rbterm.rb
@@ -2,8 +2,8 @@
 # rbterm repo. Each release: version + sha256 update + commit + push.
 # Don't hand-edit the version/sha unless the publish flow is broken.
 cask "rbterm" do
-  version "0.3.38"
-  sha256 "d1b6ec2861be2dafc3b70b51c6c27cdf11b617027dfdda009727ac6e536d56b0"
+  version "0.3.39"
+  sha256 "d82210b6dffdf4fddf7a1c75a9a4959e8943868cd952a3533840996fa50b88c4"
 
   url "https://rbterm.ximg.app/updates/macos-arm64/rbterm-#{version}.zip"
   name "rbterm"
